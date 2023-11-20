cd396fe0b935591b828105e52942750d29ca3c0a
Hossain Razavi <hossain.razavi@gmail.com>
2023-11-20 11:11:00 +0330

    ready for publication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const { i18n : {language , changeLanguage} } = useTranslation()
         <h1 className="text-3xl font-bold text-center">{language==="fa" ? "کدوم" : "Which"}<br/> {language==="fa" ? "یکی؟" : "One?"}</h1>
         </div>
         </Link>
-        <div className="fixed top-0 left-6 flex gap-2">
+        <div className="fixed top-0 left-6 flex gap-2" dir="rtl">
           <button className={`p-3 rounded-b-full ${language==="en" ? "bg-white" : "bg-yellow-500"} text-black hover:bg-yellow-300 hover:text-black`} onClick={()=>changeLanguage("en")}>EN</button>
           <button className={`p-3 rounded-b-full ${language==="fa" ? "bg-white" : "bg-yellow-500"} text-black hover:bg-yellow-300 hover:text-black`} onClick={()=>changeLanguage("fa")}>FA</button>
         </div>
